d638e1534ebf8cb5f5513b8682f525739737a4be
chaben11 <82426385+chaben11@users.noreply.github.com>
2021-05-21 10:07:48 +1200

    Lag Fixed
    Lag seemed to be fixed
    Menu updated to include logos on buttons
    Animation of logo exported from blender onto desktop

--- InterfaceTesting/Taxiphysics/Taxi.gd
+++ InterfaceTesting/Taxiphysics/Taxi.gd
@@ -5,7 +5,7 @@ const STEER_LIMIT = 0.4
 
 var steer_target = 0
 
-export var engine_force_value = 400
+export var engine_force_value = 300
 
 func _physics_process(delta):
 	var fwd_mps = transform.basis.xform_inv(linear_velocity).x
@@ -33,8 +33,10 @@ func _physics_process(delta):
 				engine_force = -engine_force_value
 		else:
 			brake = 1
+			#Animation Dark Red
 	else:
 		brake = 0.0
+		#Animation Normal red
 
 	steering = move_toward(steering, steer_target, STEER_SPEED * delta)
 
