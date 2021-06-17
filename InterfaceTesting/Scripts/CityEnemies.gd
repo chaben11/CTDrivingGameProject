a705fb54a39e9fdfcb8b2e586a0f198e290c4f7f
chaben11 <82426385+chaben11@users.noreply.github.com>
2021-06-17 14:56:42 +1200

    new enemy model and interface update
    added new enemy to the second level and made changes to how the speed works.

--- InterfaceTesting/Scripts/CityEnemies.gd
+++ InterfaceTesting/Scripts/CityEnemies.gd
@@ -4,13 +4,12 @@ extends Spatial
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-
+export var EnemySpeed = 1.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$AnimationPlayer.playback_speed = EnemySpeed
 		#This Plays the main animation that starts the easy enemies driving across the road and then jumps back to their start location
-	$AnimationPlayer.play("New Anim (copy)") #Need to rename the animation - 
-
+	$AnimationPlayer.play("New Anim (copy)") #Need to rename the animation -
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
